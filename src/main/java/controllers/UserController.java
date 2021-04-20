b88c5f53817e8db0a10ea76aefe510df9cdf527e
ezzov <ezzov@yandex.ru>
2021-04-20 13:40:37 +0300

    commit

--- src/main/java/controllers/UserController.java
+++ src/main/java/controllers/UserController.java
@@ -4,9 +4,13 @@ import dao.UserDAOImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class UserController {
@@ -18,7 +22,16 @@ public class UserController {
         this.userDAO = userDAO;
     }
 
-    @GetMapping("/users")
+    @GetMapping(value = "/")
+    public String printWelcome(ModelMap model) {
+        List<String> messages = new ArrayList<>();
+        messages.add("Congratulation");
+        messages.add("It finally works");
+        model.addAttribute("messages", messages);
+        return "indexes";
+    }
+
+    @GetMapping(value = "/users")
     public String getAllUsers(Model model){
         model.addAttribute("users", userDAO.getAllUsers());
         return "users/allusers";
